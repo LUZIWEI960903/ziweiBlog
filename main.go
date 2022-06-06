a60c8bbb555472f09be8cd35219c15190524c194
ziwei <626398871@qq.com>
2022-06-06 21:22:39 +0800

    feat: add index router

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
-package ziweiBlog
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/", index)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var indexData IndexData
+	indexData.Title = "ziwei blog"
+	indexData.Desc = "This is ziwei blog"
+	jsonStr, _ := json.Marshal(indexData)
+	w.Write(jsonStr)
+}
+
+type IndexData struct {
+	Title string `json:"title"`
+	Desc  string `json:"desc"`
+}
