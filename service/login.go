05f914238e70fe11e0db3e9a59fd7d5525cf1d7a
ziwei <626398871@qq.com>
2022-06-13 19:20:14 +0800

    fix: api Login add md5 & jwt

--- service/login.go
+++ service/login.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 	"ziweiBlog/dao"
 	"ziweiBlog/models"
+	"ziweiBlog/utils"
 )
 
 func Login(username, passwd string) (*models.LoginRes, error) {
+	passwd = utils.Md5Crypt(passwd, "ziwei")
 	user := dao.GetUser(username, passwd)
 	if user == nil {
 		return nil, errors.New("username or passwd error")
@@ -17,8 +19,14 @@ func Login(username, passwd string) (*models.LoginRes, error) {
 	userInfo.Username = user.Username
 	userInfo.Avater = user.Avater
 
+	// create token JWT
+	token, err := utils.Award(&user.Uid)
+	if err != nil {
+		return nil, errors.New("create Token error")
+	}
+
 	var ls = &models.LoginRes{
-		Token:    "",
+		Token:    token,
 		UserInfo: userInfo,
 	}
 
